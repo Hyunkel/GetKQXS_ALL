b33a8dee071a8a42c5c9b521e4e490c15240fac4
Hyunkel <dhyunkel@gmail.com>
2017-12-11 17:40:10 +0700

    Upload

--- Unittest_Python/UnittestUrl.py
+++ Unittest_Python/UnittestUrl.py
@@ -2,7 +2,6 @@ import requests
 import json
 import unittest
 import CustomAssert
-import datetime
 import xmlrunner
 
 
@@ -13,62 +12,67 @@ class UnittestSuper(unittest.TestCase, CustomAssert.CustomAssertions):
     def tearDown(self):
         print("Don dep cac truong hop cua TestUnitest")
 
-class TestURL(UnittestSuper):
+class TestURL_MB(UnittestSuper):
     global url
     global r
     url = "http://localhost:3000/kqxsmb?id=8-11-2017"
-    # request to URL
     r = requests.get(url)
-    # check status code
-    def check_stt(self):
-        self.check_stt_code(self, r)
+    def test_stt_code(self):
+        # check status code
+        self.check_stt_code(r)
     
-    # check data response
-    def check_data_rs(self):
+    def test_data_rs(self):
         self.assertTrue(r.content, "Khong Co Data")
     
-    def check_data_vali(self):
+    def test_data_vali(self):
         # check data valid json
         self.check_json(r.content)
     
-    def check_struc_data(self):
+    def test_struc_data(self):
         # check structure data
         self.check_data_full_mb(r.content)
-    
-    def runTest(self):
-        self.check_stt()
-        self.check_data_rs()
-        self.check_data_vali()
-        self.check_struc_data()
 
+#
 class TestURL_MN(UnittestSuper):
     global url2
     global r2
     url2 = "http://localhost:3000/kqxsmn/kqxshcm?id=16-11-2017"
     r2 = requests.get(url2)
-    def check_stt(self):
-        self.check_stt_code(self, r2)
+    def test_stt_code(self):
+        # check status code
+        self.check_stt_code(r2)
     
-    def check_data_rs(self):
+    def test_data_rs(self):
         self.assertTrue(r2.content, "Khong Co Data")
     
-    def check_vali_data(self):
+    def test_data_vali(self):
+        # check data valid json
         self.check_json(r2.content)
     
-    def check_data(self):
+    def test_struc_data(self):
+        # check structure data
         self.check_data_full_mk(r2.content)
-    
-    def runTest(self):
-        self.check_stt()
-        self.check_data_rs()
-        self.check_vali_data()
-        self.check_data()
 
-def suite():
-    suite = unittest.TestSuite()
-    suite.addTest(TestURL())
-    suite.addTest(TestURL_MN())
-    return suite
+
+class TestURL_MT(UnittestSuper):
+    global url3
+    global r3
+    url3 = "http://localhost:3000/kqxsmt/kqxsqt?id=16-11-2017"
+    r3 = requests.get(url3)
+    def test_stt_code(self):
+        # check status code
+        self.check_stt_code(r3)
+    
+    def test_data_rs(self):
+        self.assertTrue(r3.content, "Khong Co Data")
+    
+    def test_data_vali(self):
+        # check data valid json
+        self.check_json(r3.content)
+    
+    def test_struc_data(self):
+        # check structure data
+        self.check_data_full_mk(r3.content)
 
 if __name__ == "__main__":
     unittest.main(testRunner=xmlrunner.XMLTestRunner(output='./Demo_xml'))
